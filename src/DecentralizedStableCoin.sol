9158f89e3dfa6e15feae61034ee48579bf2fe91d
guelowrd <gaylord.warner@gmail.com>
2023-06-27 15:16:10 +0100

    first commi

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title Decentralized Stable Coin
+ * @author Gaylord Warner
+ * @dev A stable coin that is backed by a basket of other stable coins
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * This is the contract meant to be governed by DSCEngine. This is just the ERC20 implementation of our stablecoin system.
+ *
+ */
+
+contract DecentralizedStableCoin {
+
+}
